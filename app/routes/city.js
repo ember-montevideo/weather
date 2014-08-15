a49e96211c4939d2301271d8d66402d9b5847be4
Adrian Mugnolo <adrian@wyeworks.com>
2014-08-14 22:46:26 -0300

    Get conditions

--- app/routes/city.js
+++ app/routes/city.js
@@ -2,8 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return {
-      id: params.id
-    }
+    return $.ajax('https://query.yahooapis.com/v1/public/yql', {
+      data: {
+        q: 'select item from weather.forecast where woeid=' + params.id + ' and u="c"',
+        format: 'json'
+      }
+    }).then(function(response) {
+      return {
+        title: response.query.results.channel.item.title,
+        description: response.query.results.channel.item.description
+      };
+    });
   }
 });
