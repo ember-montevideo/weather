0b3c52387c497de8b543ad5dd8efe08efdb2eb02
Adrian Mugnolo <adrian@wyeworks.com>
2014-09-12 16:01:03 -0300

    Add store cache

--- app/models/store.js
+++ app/models/store.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
+var cache = {};
+
 export default Ember.Object.extend({
   find: function(name, id) {
+    if (cache[name] && cache[name][id]) {
+      return cache[name][id];
+    }
     var adapter = this.container.lookup('adapter:' + name);
-    return adapter.find(name, id);
+    return adapter.find(name, id).then(function(model) {
+      cache[name] = cache[name] || {};
+      cache[name][id] = model;
+      return model;
+    });
   }
 });
